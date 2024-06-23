0fdf603f47a6f52c17f42c2c5b48cb332b98e053
aLinDC <32669527+aLinDC@users.noreply.github.com>
2024-06-23 19:24:18 +0300

    Add files via upload

---
+++ Tema5.js
@@ -0,0 +1,5 @@
+const numbers = [4, 5, 3, 6, 25, 11, 10];
+const numbersDublate = numbers.map((number) => Math.pow(number, 2));
+console.log(numbers);
+console.log("-------------");
+console.log(numbersDublate);
