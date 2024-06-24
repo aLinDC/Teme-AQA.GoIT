91a3a81e4598da94d12240aac61f4a437f10a990
aLinDC <32669527+aLinDC@users.noreply.github.com>
2024-06-24 10:15:27 +0300

    Add files via upload

---
+++ Tema5_revizuita.js
@@ -0,0 +1,5 @@
+const numbers = [4, 5, 3, 6, 25, 11, 10];
+const numereDublate = numbers.map((number) => Math.imul(number, 2));
+console.log(numbers);
+console.log("-----------");
+console.log(numereDublate)
